dc6f7b013122a560fbd41d5387a9ed2e5dc017ac
Poojasri <vypoojasri@deloitte.com>
2024-10-17 15:16:39 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let tasks = [];
+
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
+    const text = taskInput.value.trim();
+
+    if(text){
+        tasks.push({ text: text, completed: false});
+        taskInput.value="";
+        updateTasksList();
+    }
+};
+
+const updateTasksList = () => {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task,index) => {
+        const listItem = document.createElement('li');
+        
+        listItem.innerHTML = `
+        <div class = "taskItem">
+            <div class = "task ${task.completed ? "completed": ""}">
+                <input type = "checkbox" class="checkbox" ${
+                    task.completed ? "checked" : ""
+                }/>
+                <p> ${task.text}</p>
+            </div>
+            <div class="icons">
+                <img src="edit_icon.jpg" onClick="editTask(${index})"/>
+                <img src="delete_icon.jpg" onClick="deleteTask(${index})"/>
+            </div>
+        </div>
+        `;
+
+        listItem.addEventListener("change", () => toggleTastComplete(index));
+        taskList.append(listItem);
+
+    });
+};
+
+document.getElementById("newTask").addEventListener("click",function(e){
+        e.preventDefault();
+
+        addTask();
+    }
+);
